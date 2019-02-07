a36d4460cd8896512f3dc335a12f5f441c2430ae
ishohidul <hridoy.jurain@yahoo.com>
2019-02-07 10:39:01 +0600

    init

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CarService } from './shared/car/car.service';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
+  ],
+  providers: [CarService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
+
